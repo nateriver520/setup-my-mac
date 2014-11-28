e67279ae8ec0fd3aae7db258413b0212ce2ec397
yangshuo <yangshuo@yahoo-inc.com>
2014-11-28 17:57:22 +0800

    [enhancement] add mysql

--- run.sh
+++ run.sh
@@ -6,9 +6,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 
 # software
-brew cask install appcleaner sublime-text skitch mplayerx the-unarchiver evernote qq thunder qqinput
+brew cask install appcleaner sublime-text skitch mplayerx the-unarchiver evernote qq thunder qqinput netease-music
 brew cask install intellij-idea-ce
-
 
 #env
 brew install git-extras
@@ -26,5 +25,13 @@ npm install -g grunt-cli
 npm install -g bower
 npm install supervisor -g
 
+# z
+brew install z
+echo '. `brew --prefix`/Cellar/z/1.8/etc/profile.d/z.sh' >> ~/.zshrc
 
-
+# mysql
+brew install mysql
+unset TMPDIR
+mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)"
+mysql.server start
+/usr/local/opt/mysql/bin/mysqladmin -u root password '123456'
